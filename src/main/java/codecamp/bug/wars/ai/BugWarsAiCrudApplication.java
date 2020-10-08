f255f4eb66f67d2c37aec9e519b281660a8ed1a1
Jerry Balderas <Gerardo.Balderas@cognizant.com>
2020-10-08 00:27:55 -0500

    First commit

---
+++ src/main/java/codecamp/bug/wars/ai/BugWarsAiCrudApplication.java
@@ -0,0 +1,13 @@
+package codecamp.bug.wars.ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BugWarsAiCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BugWarsAiCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/codecamp/bug/wars/ai/BugWarsAiCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package codecamp.bug.wars.ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BugWarsAiCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
