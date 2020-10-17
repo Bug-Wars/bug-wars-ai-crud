15bbcf959d975c3ba682db88d76ce6e41929feb9
Jerry Balderas <Gerardo.Balderas@cognizant.com>
2020-10-17 11:50:21 -0500

    BW-54 test

--- src/main/java/codecamp/bug/wars/ai/BugWarsAiCrudApplication.java
+++ src/main/java/codecamp/bug/wars/ai/BugWarsAiCrudApplication.java
@@ -3,6 +3,7 @@ package codecamp.bug.wars.ai;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// comment
 @SpringBootApplication
 public class BugWarsAiCrudApplication {
 
