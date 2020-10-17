296f7abb5110961c7a106f90f5ef89988d85c4f0
Jerry Balderas <Gerardo.Balderas@cognizant.com>
2020-10-17 12:16:34 -0500

    Remove comment

--- src/main/java/codecamp/bug/wars/ai/BugWarsAiCrudApplication.java
+++ src/main/java/codecamp/bug/wars/ai/BugWarsAiCrudApplication.java
@@ -3,7 +3,6 @@ package codecamp.bug.wars.ai;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-// comment
 @SpringBootApplication
 public class BugWarsAiCrudApplication {
 
