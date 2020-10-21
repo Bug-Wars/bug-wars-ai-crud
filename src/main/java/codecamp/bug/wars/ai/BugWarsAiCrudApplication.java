abb19f682fa4c1b0b94c7b4707b1e2886a664537
Jerry Balderas <Gerardo.Balderas@cognizant.com>
2020-10-20 21:48:17 -0500

    Clean up unused import

--- src/main/java/codecamp/bug/wars/ai/BugWarsAiCrudApplication.java
+++ src/main/java/codecamp/bug/wars/ai/BugWarsAiCrudApplication.java
@@ -2,7 +2,6 @@ package codecamp.bug.wars.ai;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 
 @SpringBootApplication
