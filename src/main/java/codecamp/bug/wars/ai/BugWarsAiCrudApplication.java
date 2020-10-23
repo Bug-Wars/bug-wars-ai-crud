3e90637a012f2884a8562d458b526c41167c88f7
Jerry Balderas <Gerardo.Balderas@cognizant.com>
2020-10-22 22:29:23 -0500

    Enable Eureka Client

--- src/main/java/codecamp/bug/wars/ai/BugWarsAiCrudApplication.java
+++ src/main/java/codecamp/bug/wars/ai/BugWarsAiCrudApplication.java
@@ -2,8 +2,9 @@ package codecamp.bug.wars.ai;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-
+@EnableEurekaClient
 @SpringBootApplication
 public class BugWarsAiCrudApplication {
 
