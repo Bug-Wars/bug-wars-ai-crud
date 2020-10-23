2e2ec0bb9f3a66c10fafb0f1f5e32cb53dff2f5d
mlyannaccone <meagan@yannaccone.net>
2020-10-23 10:26:39 -0400

    Setting up Zuul gateway

--- src/main/java/codecamp/bug/wars/ai/BugWarsAiCrudApplication.java
+++ src/main/java/codecamp/bug/wars/ai/BugWarsAiCrudApplication.java
@@ -2,8 +2,10 @@ package codecamp.bug.wars.ai;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableDiscoveryClient
 @EnableEurekaClient
 @SpringBootApplication
 public class BugWarsAiCrudApplication {
