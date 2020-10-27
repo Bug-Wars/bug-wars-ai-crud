1e074effd5d861aa1de0f367b7617536b948c737
mlyannaccone <meagan@yannaccone.net>
2020-10-27 13:34:24 -0400

    Adding error codes to AIController

--- src/main/java/codecamp/bug/wars/ai/controller/AIController.java
+++ src/main/java/codecamp/bug/wars/ai/controller/AIController.java
@@ -4,13 +4,15 @@ import codecamp.bug.wars.ai.exceptions.InvalidInputException;
 import codecamp.bug.wars.ai.exceptions.NameUnavailableException;
 import codecamp.bug.wars.ai.model.AIScript;
 import codecamp.bug.wars.ai.model.AIScriptResponse;
+import org.springframework.http.HttpStatus;
 import codecamp.bug.wars.ai.service.AIService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = {"http://localhost:4200", "http://localhost:8080"})
 @RestController
-@RequestMapping
 public class AIController {
     private AIService aiService;
 
@@ -19,19 +21,24 @@ public class AIController {
     }
 
     @GetMapping("/ai")
-    public List<AIScript> getAllAI() {
-        return aiService.getAllAI();
+    public ResponseEntity<List<AIScript>> getAllAI() {
+        return ResponseEntity.ok(aiService.getAllAI());
     }
 
     // Json mapper  sets up the key value pairs into a map
     // Then it uses reflection to create the object and then to populate it.
  @PostMapping("/ai")
- public AIScriptResponse createAIScript(@RequestBody AIScript script){
+ public ResponseEntity<AIScriptResponse> createAIScript(@RequestBody AIScript script){
         try {
             AIScript savedScript = aiService.saveAI(script);
-            return new AIScriptResponse(savedScript, null);
-        } catch (InvalidInputException | NameUnavailableException e){
-            return new AIScriptResponse(script, e.getMessage());
+            return ResponseEntity.ok(new AIScriptResponse(savedScript, null));
+        } catch (InvalidInputException e){
+
+            return new ResponseEntity(new AIScriptResponse(script, e.getMessage()), HttpStatus.BAD_REQUEST);
+        }
+        catch (NameUnavailableException e){
+
+        return new ResponseEntity(new AIScriptResponse(script, e.getMessage()), HttpStatus.CONFLICT);
         }
 
  }
