87d053f6c798f5c2520fb074febe7505a5734938
Jerry Balderas <Gerardo.Balderas@cognizant.com>
2020-10-29 00:46:02 -0500

    Add debug info to index

--- src/main/java/codecamp/bug/wars/ai/controller/IndexController.java
+++ src/main/java/codecamp/bug/wars/ai/controller/IndexController.java
@@ -1,13 +1,33 @@
 package codecamp.bug.wars.ai.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+
 @RestController
 public class IndexController {
+    @Value("${eureka.instance.hostname:}")
+    String hostname;
+
+    @Value("${eureka.client.serviceUrl.defaultZone:}")
+    String eurekaUrl;
+
+    @Value("${spring.profiles.active:default}")
+    String profile;
+
+    @Value("${domain.name:}")
+    String domainName;
 
     @GetMapping
     public String getIndex() {
-        return "bug-wars-ai-crud";
+        return String.join("<BR>", Arrays.asList(
+                "Bug Wars AI CRUD Repository",
+                "Hostname: " + hostname,
+                "Eureka hostname; " + eurekaUrl,
+                "Spring Profile: " + profile,
+                "DOMAIN NAME: " + domainName
+        ));
     }
 }
